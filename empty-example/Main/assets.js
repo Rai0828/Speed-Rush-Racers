32612406dd4238cdb7446895eaa8ccb974eaec6e
railei <raileidugayo8220@gmail.com>
2026-03-04 13:34:09 +0800

    restore

--- empty-example/Main/assets.js
+++
@@ -1,6 +0,0 @@
-function preloadAssets() {
-  playerImg = loadImage("assets/motorcycle.png");
-  carImgs[0] = loadImage("assets/car1.png");
-  carImgs[1] = loadImage("assets/car2.png");
-  themeSound = loadSound("assets/theme.mp3");
-}
